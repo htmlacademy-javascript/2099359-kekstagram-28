0dd6100a5b26d2474f39c32449464ec10aa9644c
Олеся Панфилова <olesapanfilova274@gmail.com>
2023-02-21 20:41:38 +0800

    Функции

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// function lastNumber(number) {
+
+//   if (number <= 10000){
+
+//   return number % 10;
+
+//   }
+
+// }
+
+// console.log(lastNumber('999'))
+
+
+// function firstNumber (number) {
+//   return Math.floor(number / 10 % 10)
+
+// }
+// console.log(firstNumber('98'))
