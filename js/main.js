e89648aafde51f70e5056d71c07010506a04a14d
Олеся Панфилова <olesapanfilova274@gmail.com>
2023-02-28 13:54:00 +0800

    Правка

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // итоговый массив
-let dataBases = [];
+const dataBases = [];
 
 // тексты комментариев
-let messages = [
+const messages = [
   'Всё отлично!', 'В целом всё неплохо.', 'Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
   'В конце концов это просто непрофессионально.',
@@ -13,20 +13,20 @@ let messages = [
 ];
 
 // описания к фотографиям
-let descriptions = [
+const descriptions = [
   'Зимнее утро.', 'Сказочный закат.', 'Чудесный тёплый вечер',
   'Я и моя семья.', 'Подарок на 8 марта.', 'Прогулка в лесу.',
   'Поход в ресторан.'
 ];
 
 // имена комментаторов
-let names = [
+const names = [
   'Иван', 'Георгий', 'Олег', 'Роман',
   'Александр', 'Валентина', 'Олеся',
   'Евдокия', 'Дмитрий'
 ];
 
-let getRandomInteger = (a, b) => {
+const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
@@ -35,11 +35,11 @@ let getRandomInteger = (a, b) => {
 
 let commentId = 100;
 function getRandomComments(quantity){
-  let comments = [];
+  const comments = [];
   for (let idx = 1; idx <= quantity; idx++) {
     comments[idx - 1] = {
       id: commentId += 1,
-      avatar: 'img/avatar-' + getRandomInteger(1, 6) + '.svg',
+      avatar: `img/avatar-, ${getRandomInteger(1, 6)} + '.svg`,
       message: messages[getRandomInteger(0, messages.length - 1)],
       name: names[getRandomInteger(0, names.length - 1)],
     };
@@ -51,13 +51,12 @@ const createRhoto = () => {
   for(let idx = 1; idx <= 25; idx++) {
     dataBases[idx - 1] = {
       id: idx,
-      url: 'photos/' + idx + '.jpg' ,
+      url: `photos/ ${idx} + '.0jpg` ,
       description: descriptions[getRandomInteger(0, descriptions.length - 1)],
       likes: getRandomInteger(15, 200),
       comments: getRandomComments(getRandomInteger(1, 3))
     };
   }
-  console.log(dataBases)
 };
 createRhoto();
 
