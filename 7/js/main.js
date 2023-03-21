ba64205ad697a9895ad4248a283c5d5e88e40fb4
Keks <keks@htmlacademy.ru>
2023-03-21 17:02:26 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,9 @@
+import {generatePhoto} from './data.js';
+// import {renderGallery} from './gallery.js';
+
+
+// renderGallery(generatePhoto());
+
+import {renderPictureModal} from './gallery.js';
+
+renderPictureModal(generatePhoto());
---
+++ 7/js/util.js
@@ -0,0 +1,15 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomElement = (arr) => {
+  const idx = getRandomInteger(0, arr.length - 1);
+  return arr[idx];
+};
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export { getRandomInteger, getRandomElement, isEscapeKey };
