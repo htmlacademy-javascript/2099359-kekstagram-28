e356ab55758571245a1858058d97e788f3ee1ac9
Keks <keks@htmlacademy.ru>
2023-03-23 15:41:39 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/main.js
+++ 7/js/main.js
@@ -1,9 +1,4 @@
 import {generatePhoto} from './data.js';
-// import {renderGallery} from './gallery.js';
-
-
-// renderGallery(generatePhoto());
-
 import {renderPictureModal} from './gallery.js';
 
 renderPictureModal(generatePhoto());
