daee3cd409e67ab192fbcc46b2408374d000199b
Олеся Панфилова <olesapanfilova274@gmail.com>
2023-04-13 18:43:42 +0800

    Фото на память=)

--- js/effects.js
+++ js/effects.js
@@ -52,7 +52,7 @@ const EFFECTS = [
 const DEFAULT_EFFECT = EFFECTS[0];
 let chosenEffect = DEFAULT_EFFECT;
 
-const imageElement = document.querySelector('.img-upload__preview img');
+export const imageElement = document.querySelector('.img-upload__preview img');
 const effectsElement = document.querySelector('.effects');
 const sliderElement = document.querySelector('.effect-level__slider');
 const sliderContainer = document.querySelector('.img-upload__effect-level');
